29f2a25f125d3727a03445ad442209adaa0e94aa
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2023-04-07 22:19:36 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='titlebar'></div>
+      Hello World
+    </div>
+  )
+}
+
+export default App
